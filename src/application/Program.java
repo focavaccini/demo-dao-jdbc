e284015a6ff550991dc14d3032ea6a246f0551f3
Fernando Cavaccini <fernandocavaccini@gmail.com>
2021-12-03 17:34:22 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Jogos");
-		System.out.println(obj);
+		Seller seller = new Seller(1, "Caveira", "caveirao@gmail.com", new Date(), 3500.0, obj);
+		System.out.println(seller);
+		
+		
 	}
 }
